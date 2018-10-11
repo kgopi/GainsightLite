986209245a795a84d59c9b344137b84f46022664
Naveen Ithapu <nithapu@gainsight.com>
2018-10-11 19:49:39 +0530

    push notification android

--- android/app/src/main/java/com/gainsightlite/MainApplication.java
+++ android/app/src/main/java/com/gainsightlite/MainApplication.java
@@ -3,6 +3,7 @@ package com.gainsightlite;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.auth0.react.A0Auth0Package;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new A0Auth0Package(),
           new VectorIconsPackage()
       );
